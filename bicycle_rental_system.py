a69d93f827e0f0e67e29352c4dc0686cb4bd6ab8
MASKY-33 <masky.somebody33@gmail.com>
2026-06-25 16:43:52 +0200

    Create bicycle_rental_system.py

---
+++ bicycle_rental_system.py
@@ -0,0 +1,23 @@
+# P.S. Building this Project-System really took me mentally deep for a whole afternoon until the next morning to get it fully working.
+# It was so terrible that it was amazing!
+# It was purely because I was searching for the correct System-architecture (flows), for making it work correcty.
+
+
+rentable_bicycles = {
+    "stadsfiets" : 120.00,
+    "e-bike" : 249.99,
+    "mountainbike" : 70.00
+}
+
+while True:
+    bicycle_search = input("Which bicycle are you searching for?: ").lower()
+
+    if bicycle_search == "quit":
+        print("Thankyou, goodbye!")
+        break
+
+    if bicycle_search in rentable_bicycles:
+        bicycle_price = rentable_bicycles[bicycle_search]
+        print(f"bicycle price is: {bicycle_price}")
+    else:
+        print("Bicycle is not available")
